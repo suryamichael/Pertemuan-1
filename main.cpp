29c8124c14f61f59b708f46eb2f36737f71df7aa
suryamichael <112886206+suryamichael@users.noreply.github.com>
2022-09-05 21:43:02 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "MKichael Surya" << endl;
+    cout << "A11.2022.14124" << endl;
+    cout << "Gg glodok baru genuksari" << endl;
+    cout << "SMA PIUS TEGAL" << endl;
+    return 0;
+}
