6f4685b327291294af340152f0e5d3bbc356ce9a
suryamichael <112886206+suryamichael@users.noreply.github.com>
2022-09-12 15:20:01 +0700

    Add files via upload

---
+++ Pertemuan 3 12 sep 2022/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int X;
+    X=18;
+    int Y =3;
+    string K = "xl";
+    float z = 178.5;
+
+    cout<<"\nUmur Saya = "<< X ;
+    cout<<"\nTinggi Saya = "<< z ;
+    cout<<"\nUkuran Baju = "<< K ;
+    return 0;
+}
+
