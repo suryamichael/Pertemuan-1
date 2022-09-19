c47926e75f0733ec6939443b3011cb3ea3b08d74
suryamichael <112886206+suryamichael@users.noreply.github.com>
2022-09-19 22:34:08 +0700

    Add files via upload

---
+++ Pertemuan 5/19 sep 22.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int panjang;
+    int lebar;
+    int jari jari;
+
+    cout <<"\tMenu\n\n";
+
+    clang --version.
+}
---
+++ Pertemuan 5/19 september 22/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float panjang, lebar, jari;
+    int pilihan;
+    float pi = 3.14;
+    char ulang;
+
+    do {
+        cout << "RUMUS BANGUN DATAR!!!!!"<< endl;
+        cout <<"----------------------"<< endl;
+        cout <<"pilih rumus"<< endl;
+        cout <<"1. Persegi Panjang"<< endl;
+        cout <<"2. lingkaran"<< endl;
+        cout << endl;
+
+        cout << "PILIH RUMUS:" << endl;
+        cin >> pilihan;
+
+        switch (pilihan) {
+        case 1:
+            cout << "masukkan Panjang:" << endl;
+            cin >> panjang;
+            cout << "masukkan Lebar" << endl;
+            cin >> lebar;
+            cout <<"Luas dari persegi panjang adalah:"<<panjang * lebar << endl;
+            break;
+        case 2:
+            cout << "masukkan jari"<<endl;
+            cin >> jari;
+            cout <<"Luas dari Lingkaran adalah:"<<jari * jari / pi << endl;
+            break;
+        default:
+            cout << "Apa kuwi?" << endl;
+        }
+        cout << endl;
+
+        cout << "Ingin memilih rumus laen? (y/t)? ";
+        cin >> ulang;
+        cout << endl;
+  }
+    while (ulang!= 't');
+
+    cout << "Dimengerti semnoga harimu menyenangkan!!!";
+
+    cout << endl;
+    return 0;
+}
