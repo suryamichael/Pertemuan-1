4fdee7e90a4e394fcb73e4c9367c364b4efab084
suryamichael <112886206+suryamichael@users.noreply.github.com>
2022-11-08 20:11:23 +0700

    Update main.cpp

--- Pertemuan 15/main.cpp
+++ Pertemuan 15/main.cpp
@@ -11,9 +11,10 @@ int main()
     cout << "Ayo bantu dia untuk menghitung berapa permen yang bisa dimiliki nya!!" << endl;
     cout << "(1) JIka harga permen 500 maka ia akan mengambil 5 permen saja" << endl;
     cout << "(2) Jika harga permen 1000 maka ia akan menghabis seluruh uang nya" << endl;
-    cout << "(3) Jika harga permen 300 maka ia akan menghabis seluruh uang nya" << endl;
+    cout << "(3) Jika harga permen 300 maka ia akan menghitungnya mulai dari 300, 600, 900, .... terus hingga uangnya habib" << endl;
     cout << "Mau pilih yang mana dulu??";
     cin >> x;
+    cout << endl;
 
     switch (x) {
     case 1:
@@ -26,9 +27,19 @@ int main()
         cout << "Dengan harga 1000, Dito mampu membeli permen sebanyak:" << z;
         break;
     case 3:
-        y = 300;
-        z = b/y;
-        cout << "Dengan harha 300, Dito mampu membeli permen sebanyak:" << z;
+        y = 0;
+        z= 0;
+        cout <<"Dito mulai menghitung jumlah permen yg bisa didapat:" << endl;
+        while (y < 4700){
+            z++;
+            y+=300;
+            cout << y;
+            if (y < 4700) {
+            cout <<", ";
+            }
+        } cout <<".";
+        cout << endl;
+        cout << "Dengan harga 300, Dito mampu membeli permen sebanyak:" <<z ;
         break;
 
         default:
