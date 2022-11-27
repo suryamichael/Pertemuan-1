b3134eac6cb5904e534a5f637c9ab3c204ac8bda
suryamichael <112886206+suryamichael@users.noreply.github.com>
2022-11-28 00:06:20 +0700

    Add files via upload

--- Pertemuan 17/Loop13/main.cpp
+++ Pertemuan 17/Loop13/main.cpp
@@ -6,19 +6,50 @@ int main()
 {
     int x;
     float y, z;
-    z = -50;
-    cout <<"Tabel Konversi suhu dari celsius ke fahrenheit"<< endl;
-    cout <<"=============================================="<< endl;
-    cout << "Celsius" << "          " << "Fahrenheit" << "        " << "Keterangan" << endl;
-    cout <<"=============================================="<< endl;
 
-    for (x = 0; x < 100; x++) {
-        if ( x  == 1) {
-            cout << "Bang dingin bang" << endl;
-        }
+    cout << "PROGRAM CONVERTER SUHU BESERTA KETERANGAN" << endl;
+    cout << endl;
+    cout << endl;
+
+    cout << "===============================================" << endl;
+    cout << "Celsius" << "        " << "Fahrenheit" << "          " << "Keterangan" << endl;
+    cout << "===============================================" << endl;
+
+    for ( x = -50; x <= 100; x++) {
+            z = x;
+        if ( z == -40) {
+            y = (z * 9/5) + 32;
+            cout << x << "              " << y << "              " << "Cuaca dingin sangat bersalju" << endl;
+        } else if (z == -18) {
+            y = (z * 9/5) + 32;
+            cout << x << "            " << y << "          " << "Cuaca dingin bersalju" << endl;
+        } else if (z == 0) {
+            y = (z * 9/5) + 32;
+            cout << x << "                " << y << "               " << "Titik beku air" << endl;
+        } else if (z == 10) {
+            y = (z * 9/5) + 32;
+            cout << x << "               " << y << "               " << "Hari yang dingin" << endl;
+        } else if (z == 21) {
+            y = (z * 9/5) + 32;
+            cout << x << "               " << y << "             " << "Temperatur Ruangan" << endl;
+        } else if (z == 30) {
+            y = (z * 9/5) + 32;
+            cout << x << "               " << y << "               " << "Cuaca Pantai" << endl;
+        } else if (z == 37) {
+            y = (z * 9/5) + 32;
+            cout << x << "               " << y << "             " << "Suhu Tubuh" << endl;
+        } else if (z == 40) {
+            y = (z * 9/5) + 32;
+            cout << x << "               " << y << "              " << "Air mandi panas" << endl;
+        } else if (z == 100) {
+            y = (z * 9/5) + 32;
+            cout << x << "              " << y << "              " << "Air mendidih" << endl;
+        } else {continue;}
+
     }
+    cout << endl;
 
-    cout << "Michael Surya" << endl;
-    cout << "A11.2022.14124" << endl;
+        cout <<"Michael Surya" << endl;
+        cout << "A11.2022.14124" << endl;
     return 0;
 }
